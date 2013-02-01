a19807a6a08889cf231c32968e57fc0d97329b85
phamonyut <phamonyut@gmail.com>
2013-02-01 16:48:03 +0700

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoorayjob::Application.config.secret_token = '03827166fcdb8a6bc426e364e2a944b60711b5aacfcb0388e4881485164e0d4350a6231f6b4ad5068b873a18569033357f331aa58b13626dec38bfa905f0ea0e'
