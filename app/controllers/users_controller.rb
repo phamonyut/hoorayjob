b1027070c3049bfd3d2be488a878ebaffd9d030e
phamonyut <phamonyut@gmail.com>
2013-02-06 16:01:30 +0700

    add create user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,18 @@ class UsersController < ApplicationController
 	# POST /users
 	def create
 		puts "-- call create user"
+
+		@user = User.new(params[:user])
+		
+		respond_to do |format|
+			if @user.save
+				format.html {redirect_to root_path, notice: "User was successfully created."}
+			else
+				format.html {render :action => "registration"}
+				format.json {render json: @user.errors, status: :unprocessable_entity }
+			end
+		end
+		
 	end
 
 end
