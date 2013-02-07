d0fe576bd961ba0142c4aaf0a197ed76d8398825
phamonyut <phamonyut@gmail.com>
2013-02-07 13:54:42 +0700

    add filter before call hello action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,16 @@
 class HomeController < ApplicationController
 
+	before_filter :signed_in_user, only: [:hello]
+
 	def index
 	end
 
 	def hello
 	end
+
+	private
+		def signed_in_user
+			redirect_to root_path, notice: "Please sign in." unless user_signed_in?
+		end
 
 end
