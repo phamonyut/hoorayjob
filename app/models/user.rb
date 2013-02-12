3c1a5ef4aa0a4f25bf37e643cda16fe8f65908f0
kritakorn.kritmanorote <kritakorn.kritmanorote@dstworldwideservices.com>
2013-02-12 16:32:03 +0700

    Add validation of the citizen_id back (but no more required)

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   validates :user_type, presence: true
  	
   VALID_DIGIT_REGEX = /\A\d+\Z/
-  # validates	:citizen_id, presence: true, format: { with: VALID_DIGIT_REGEX }, uniqueness: true
+  validates	:citizen_id, format: { with: VALID_DIGIT_REGEX }, uniqueness: true, allow_blank: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }, allow_blank: true
