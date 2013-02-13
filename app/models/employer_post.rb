c1c964e77970f496eab1e626388c8f0959f4a59c
phamonyut <phamonyut@gmail.com>
2013-02-13 14:53:03 +0700

    add desc to attr_accessible

--- app/models/employer_post.rb
+++ app/models/employer_post.rb
@@ -1,6 +1,5 @@
 class EmployerPost < ActiveRecord::Base
-  attr_accessible :district, :job_id, :max_age, :max_salary, :min_age, :min_salary, :province, :sex, :status, :user_id, :year_of_experience
-  
+  attr_accessible :district, :job_id, :max_age, :max_salary, :min_age, :min_salary, :province, :sex, :status, :user_id, :year_of_experience, :desc  
   belongs_to :user
   belongs_to :job
   belongs_to :sex
