bb135139e4ba9883b538300881dd8a8456233414
phamonyut <phamonyut@gmail.com>
2013-02-19 14:00:03 +0700

    fix factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -12,11 +12,11 @@ FactoryGirl.define do
 		username "username"
 		password "foobar"
 		password_confirmation "foobar"
-		user_type UserType.find_by_name("individual")
+		user_type { UserType.find_by_name("individual") }
 		citizen_id "1234567890000"
 		first_name "Firstname"
 		last_name "Lastname"
-		sex Sex.find_by_name("male")
+		sex { Sex.find_by_name("male") }
 		birthday "2013-02-06"
 		phone "080-123-4567"
 		email "fullbar@email.com"
@@ -30,26 +30,26 @@ FactoryGirl.define do
 
 	factory :employee_post do
 		user
-		job Job.find_by_job_name("housekeeper")
+		job { Job.find_by_job_name("housekeeper") }
 		status "O"
 		min_salary 10000
 		max_salary 15000
 		year_of_experience 3
-		district District.find_by_name("ดินแดง")
-		province Province.find_by_name("กรุงเทพมหานคร")
+		district { District.find_by_name("ดินแดง") }
+		province { Province.find_by_name("กรุงเทพมหานคร") }
 	end
 
 	factory :employer_post do
 		user
-		job Job.find_by_job_name("housekeeper")
+		job { Job.find_by_job_name("housekeeper") }
 		status "O"
 		min_salary 12000
 		max_salary 14000
 		year_of_experience 5
-		sex Sex.find_by_name("male")
+		sex { Sex.find_by_name("male") }
 		min_age 25
 		max_age 30
-		district District.find_by_name("ดินแดง")
-		province Province.find_by_name("กรุงเทพมหานคร")
+		district { District.find_by_name("ดินแดง") }
+		province { Province.find_by_name("กรุงเทพมหานคร") }
 	end
 end
