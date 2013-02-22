8e4b5de2ade65cca5b39481eb03fba8346708be1
phamonyut <phamonyut@gmail.com>
2013-02-22 15:20:09 +0700

    add validation firstname on signup page

--- app/assets/javascripts/signup_validation.js
+++ app/assets/javascripts/signup_validation.js
@@ -1,5 +1,6 @@
 $(function ()
 {
+  $('#user_first_name').focusout(clearFullnameMsg).bind('input', fullnameChangeHandler);
   $('#user_username').focusout(usernameFocusoutHandler)
                      .bind('input', usernameChangeHandler);
   $('#user_password').focusout(passwordFocusoutHandler)
@@ -16,8 +17,25 @@ function isEmpty(obj)
 
 function signupHandler() 
 {
+  isFullnameValid = fullnameSubmitHandler();
   isPasswordValid = passwordSubmitHandler();
-  return isPasswordValid;
+  isUsernameValid = validateUsernameOnClient($('#user_username').val());
+  return isFullnameValid && isUsernameValid && isPasswordValid;
+}
+
+function fullnameSubmitHandler()
+{
+  var valid = true;
+  var msg;
+  var fullname = $('#user_first_name').val();
+
+  clearFullnameMsg();
+  if (isEmpty(fullname)) {
+    valid = false;
+    msg = I18n.t("fullname_blank");
+    fullnameMsgHandler(valid, msg);
+  }
+  return valid;
 }
 
 function passwordSubmitHandler()
@@ -87,6 +105,17 @@ function passwordChangeHandler()
   clearPasswordMsg();
 }
 
+function fullnameChangeHandler()
+{
+  clearFullnameMsg();
+}
+
+function clearFullnameMsg()
+{
+  $('#user_first_name').removeClass('error');
+  $('#user_first_name_message').addClass('hide');
+}
+
 function clearPasswordMsg() 
 {
   $('#user_password').removeClass('error');
@@ -96,13 +125,22 @@ function clearPasswordMsg()
 
 }
 
-function passwordMsgHandler(valid, msg) {
+function fullnameMsgHandler(valid, msg) 
+{
+  $('#user_first_name').addClass('error');
+  $('#user_first_name_message').removeClass('hide');
+  $('#user_first_name_message').text(msg);
+}
+
+function passwordMsgHandler(valid, msg) 
+{
   $('#user_password').addClass('error');
   $('#user_password_message').removeClass("hide");
   $('#user_password_message').text(msg);
 }
 
-function passwordConfirmationMsgHandler(valid, msg) {
+function passwordConfirmationMsgHandler(valid, msg) 
+{
   $('#user_password_confirmation').addClass('error');
   $('#user_password_confirmation_message').removeClass("hide");
   $('#user_password_confirmation_message').text(msg);
@@ -122,6 +160,7 @@ function usernameFocusoutHandler()
     if (validateUsernameOnClient(username))
     {
       // Show the loading icon and progress text
+      $('#user_username').removeClass('error');
       $('#yes-icon').addClass("hide");
       $('#no-icon').addClass("hide");
       $('#loading-icon').removeClass("hide");
@@ -136,6 +175,7 @@ function usernameFocusoutHandler()
             // Ensure that the value in the textbox is the same as the one we sent to validate
             if( $('#user_username')[0].value == username)
             {
+              $('#user_username').removeClass('error');
               $('#yes-icon').removeClass("hide");
               $('#user_username_message').addClass("valid").text(I18n.t("username_valid"));
             }
@@ -144,6 +184,7 @@ function usernameFocusoutHandler()
           203: function(data) {
             if( $('#user_username')[0].value == username)
             {
+              $('#user_username').addClass('error');
               $('#no-icon').removeClass("hide");
               $('#user_username_message').text(I18n.t(data.error_message));
             }
@@ -153,6 +194,7 @@ function usernameFocusoutHandler()
             // Ensure that the value in the textbox is still blank
             if( $('#user_username')[0].value == "")
             {
+              $('#user_username').addClass('error');
               $('#user_username_message').text(I18n.t("errors.messages.blank"));
             }
           }
@@ -164,6 +206,7 @@ function usernameFocusoutHandler()
 
 function validateUsernameCallback()
 {
+  $('#user_username').removeClass('error');
   $('#loading-icon').addClass("hide");
   $('#user_username_message').removeClass("checking");
 }
@@ -178,6 +221,7 @@ function validateUsernameOnClient(username)
   // $ represents end of the string
   if(!username.match(/^[a-zA-Z0-9_]{1,12}$/))
   {
+     $('#user_username').addClass('error');
      $('#user_username_message').text(I18n.t("invalid_username_character")).removeClass("hide valid");
     return false;
   }
@@ -193,6 +237,7 @@ function usernameChangeHandler()
   $('#no-icon').addClass("hide");
   $('#loading-icon').addClass("hide");
   $('#user_username_message').addClass("hide")
+  $('#user_username').removeClass('error');
   validateUsernameOnClient($('#user_username')[0].value);
 }
 
